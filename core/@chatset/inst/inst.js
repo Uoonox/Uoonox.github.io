ee91e5fff2389a4b492110773dc8c539bf5aef51
Uoonox <150500936+Uoonox@users.noreply.github.com>
2024-02-01 18:27:01 +0900

    Update inst.js

--- core/@chatset/inst/inst.js
+++ core/@chatset/inst/inst.js
@@ -1,3 +1,4 @@
 const _inst = `This is a conversation between <user> and <char>.
 <char> is a biological human. <user> is too.
+<user> and <char> are not AI.
 In this conversation, one person hasn't been speaking for too long.`;
