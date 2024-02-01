07572a6b3caaf743e57748cd68235a77fe86a2c5
Uoonox <150500936+Uoonox@users.noreply.github.com>
2024-02-01 18:52:22 +0900

    Update script.js

--- core/script.js
+++ core/script.js
@@ -33,36 +33,28 @@ document.addEventListener("DOMContentLoaded", function () {
             userInput.value = "";
         }
     }
-    
+
     function sendFirstMessage() {
         let storedLog = JSON.parse(localStorage.getItem('chatLog'));
-        document.title = `Uoonox - ${char}`;
-
+    
         if (storedLog && storedLog.length > 0) {
             storedLog.forEach(logEntry => {
-                const messages = logEntry.split('\n\n').filter(message => message.trim() !== '');
-                messages.forEach(message => {
-                    let messageType;
-                    let formattedMessage;
-                    if (message.includes(`${user}:`)) {
-                        messageType = "user-message";
-                        formattedMessage = message.replace(`${user}:\n`, '');
-                    } else if (message.includes(`${char}:`)) {
-                        messageType = "ai-message";
-                        formattedMessage = message.replace(`${char}:\n`, '');
-                    }
-                    if (formattedMessage) {
-                        formattedMessage = formattedMessage.replace(/\n/g, '<br>');
-                        displayMessage(formattedMessage, messageType);
-                    }
-                });
+                const messageRegex = new RegExp(`(${user}|${char}):\\n(.*?)(?=\\n\\n(${user}|${char}):|\\n\\n$)`, 'gs');
+    
+                let match;
+                while ((match = messageRegex.exec(logEntry)) !== null) {
+                    let messageType = match[1] === user ? "user-message" : "ai-message";
+                    let formattedMessage = match[2].replace(/\n/g, '<br>');
+    
+                    displayMessage(formattedMessage, messageType);
+                }
             });
         } else {
             let formattedFirstMsg = firstmsg.replace(/\n/g, '<br>');
             displayMessage(formattedFirstMsg, "ai-message");
         }
     }
-
+    
     sendFirstMessage();
 });
 
@@ -70,4 +62,3 @@ document.getElementById('memoryDeleteButton').addEventListener('click', () => {
     localStorage.removeItem('chatLog');
     location.reload();
 });
-
